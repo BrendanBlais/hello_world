2cddd3b19c379f244af94fe5ce51e896236d617d
Brendan Blais <coolertin100@gmail.com>
2019-10-23 12:56:00 -0400

    Brendan Changed Name/Print

--- hello.java
+++ hello.java
@@ -5,10 +5,10 @@ public class hello {
     public static void main(String [] args) {
 
 
-        String firstName = "Elizabeth";
-        String lastName = "Jones";
+        String firstName = "Brendan";
+        String lastName = "Blais";
 	
-        System.out.println("Hello Class!");
+        System.out.println("Hello I'm!" + firstName + " " + lastName);
 
 
 
